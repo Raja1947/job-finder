ad2b2c53a1b8cd28cdff3b33f4801d554929662c
Raja Dhoundiyal <rajadhoundiyalpda5@gmail.com>
2025-03-01 17:39:38 +0530

    home page complete

---
+++ src/pages/JobCategory/JobCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JobCategory = () => {
+  return (
+    <div className='pt-20 pb-12'>
+      
+    </div>
+  )
+}
+
+export default JobCategory
--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import logoImage from "../../assets/logo.png";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="h-[13vh] overflow-hidden shadow-md">
       <div className="w-[90%] md:w-[80%] h-[100%] mx-auto flex items-center justify-between">
@@ -11,8 +11,17 @@ const Navbar = () => {
           <img src={logoImage} alt="logo" width={250} height={250} />
         </div>
         <div className="flex items-center space-x-4">
-          <button onClick={()=>navigate("/signup")} className="px-4 py-1.5 text-[14px] sm:text-[16px] sm:px-6 sm:[py-2] bg-blue-600 font-semibold text-white rounded-lg hover:bg-blue-800 transition-all duration-300">
+          <button
+            onClick={() => navigate("/signup")}
+            className="px-4 py-1.5 text-[14px] sm:text-[16px] sm:px-6 sm:[py-2] bg-blue-600 font-semibold text-white rounded-lg hover:bg-blue-800 transition-all duration-300"
+          >
             Sign Up
+          </button>
+          <button
+            onClick={() => navigate("/signup")}
+            className="px-4 py-1.5 text-[14px] sm:text-[16px] sm:px-6 sm:[py-2] bg-orange-600 font-semibold text-white rounded-lg hover:bg-orange-800 transition-all duration-300"
+          >
+            Post a Job
           </button>
         </div>
       </div>
